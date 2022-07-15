fadc6fb098cabe81b996d7e4c1f1d394fd12a3ec
Kasper Larsen <kasperhorn2003@gmail.com>
2022-07-15 17:07:03 +0200

    more testing

--- test.py
+++ test.py
@@ -1,54 +1,93 @@
 import torch
 import torch.nn as nn
-import torch_optimizer as optim
+import torchvision 
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-X = torch.tensor([[1],[2],[3],[4]], dtype=torch.float32)
-y = torch.tensor([[6.57],[13.14],[19.71],[26.28]], dtype=torch.float32)
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-xTest = torch.tensor([[5]], dtype=torch.float32)
+input_size = 28 * 28
+hidden_size = 64
+num_classes = 10
+epochs = 10
+batch = 64
+learning_rate = 0.001
+
+traning_dataset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False, download=False, transform=transforms.ToTensor())
+
+trainloader = torch.utils.data.DataLoader(traning_dataset, batch_size=batch, shuffle=True)
+testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch, shuffle=False)
+
+exsampels = iter(trainloader).next()
+samples, label = exsampels
+
+for i in range(6):
+    plt.subplot(2, 3, i+1)
+    plt.imshow(samples[i][0], cmap='gray')
+#plt.show()
 
 class Net(nn.Module):
     """Some Information about Net"""
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, hidden_size, num_classes):
         super(Net, self).__init__()
-        self.lr1 = nn.Linear(input_size, output_size)
+        self.lr1 = nn.Linear(input_size, hidden_size)
+        self.lr2 = nn.Linear(hidden_size, hidden_size)
+        self.lr3 = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.lr1(x)
+        x = self.lr2(self.relu(x))
+        x = self.lr3(self.relu(x))
         return x
-    
-nSam, nFea = X.shape
-print(nSam, nFea)
 
-input_size = nFea
-output_size = nFea
-model = Net(input_size, output_size)
+def acc():
+    with torch.no_grad():
+        nCorrect = 0
+        nSampels = 0
+        for inputs, labels in testloader:
+            inputs = inputs.reshape(-1, 28*28)
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+            outputs = net(inputs)
+            
+            _, predistion = torch.max(outputs, 1)
+            
+            nSampels += labels.shape[0]
+            nCorrect += (predistion == labels).sum().item()
+        acc = 100.0 * nCorrect / nSampels
+    return acc
+    
+net = Net(input_size, hidden_size, num_classes).to(device)
 
-criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
-#optimizer = optim.Apollo(model.parameters(), lr=1e-2)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=1e-2)
 
-pbar = tqdm(range(5000))
+pbar = tqdm(range(epochs), unit='epoch')
 
-for _ in pbar:
-    # Forward pass
-    y_pre = model(X)
-    
-    # Compute loss
-    loss = criterion(y, y_pre)
-    
-    # Backward pass
-    loss.backward()
-    
-    # Update weights
-    optimizer.step()
-    optimizer.zero_grad()
-    
-    if _ % 100 == 0:
-        pbar.set_description(f"Loss: {loss.item():.8f}")
-        
-with torch.no_grad():
-    y_pred = model(xTest)
-    print(f"f(5) = {y_pred.item():.3f}")
-    
+# loop over the dataset multiple times
+for epoch in pbar:
+    running_loss = 0.0
+    for i, data in enumerate(trainloader):
+        inputs, labels = data
+        inputs = inputs.reshape(-1, 28*28)
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    pbar.set_description(f"Running loss: {running_loss:.5}, Loss = {loss.item():.8}, acc: {acc()}")
+
+print('Finished Training')
+print(f"Total loss: {running_loss:.5}, acc: {acc()}")
+   
